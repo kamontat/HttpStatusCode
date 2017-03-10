af18e9852c336d09bbbf40fad37fc7010fe0e07d
kamontat <kamontat_c@hotmail.com>
2017-03-10 22:39:11 +0700

    complete http status code.

--- src/HttpStatus.java
+++ src/HttpStatus.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 /**
+ * All HTTP Responded Status Code. <br>
  * Base on wikipedia & Ben Griffiths Code
  * <ul>
  * <li><a href="https://github.com/whostolebenfrog/status-codes/blob/master/src/main/java/com/floatbackwards/statuscodes/HttpStatus.java">Github gist</a></li>
@@ -141,92 +142,336 @@ public enum HttpStatus {
 	
 	// --------------------------------------------------------------------
 	
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, too large size, invalid request message framing, or deceptive request routing).
+	 */
 	Bad_Request(400, "The request could not be understood by the server."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * Similar to 403 Forbidden, but specifically for use when authentication is required and has failed or has not yet been provided. The response must include a WWW-Authenticate header field containing a challenge applicable to the requested resource. See Basic access authentication and Digest access authentication.
+	 */
 	Unauthorized(401, "The request requires authorization."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * Reserved for future use. The original intention was that this code might be used as part of some form of digital cash or micropayment scheme, but that has not happened, and this code is not usually used. Google Developers API uses this status if a particular developer has exceeded the daily limit on requests.
+	 */
 	Payment_Required(402, "Reserved for future use."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request was valid, but the server is refusing action. The user might not have the necessary permissions for a resource.
+	 */
 	Forbidden(403, "Whilst the server did understand the request, the server is refusing to complete it. This is not an authorization problem."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible.
+	 */
 	Not_Found(404, "The requested resource was not found."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * A request method is not supported for the requested resource; for example, a GET request on a form that requires data to be presented via POST, or a PUT request on a read-only resource.
+	 */
 	Method_Not_Allowed(405, "The supplied method was not allowed on the given resource."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The requested resource is capable of generating only content not acceptable according to the Accept headers sent in the request.
+	 */
 	Not_Acceptable(406, "The resource is not able to return a response that is suitable for the characteristics required by the accept headers of the request."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The client must first authenticate itself with the proxy.
+	 */
 	Proxy_Authentication_Required(407, "The client must authenticate themselves with the proxy."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server timed out waiting for the request. According to HTTP specifications: The client did not produce a request within the time that the server was prepared to wait. The client MAY repeat the request without modifications at any later time.
+	 */
 	Request_Timeout(408, "The client did not supply a request in the period required by the server."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * Indicates that the request could not be processed because of conflict in the request, such as an edit conflict between multiple simultaneous updates.
+	 */
 	Conflict(409, "The request could not be completed as the resource is in a conflicted state."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * Indicates that the resource requested is no longer available and will not be available again. This should be used when a resource has been intentionally removed and the resource should be purged. Upon receiving a 410 status code, the client should not request the resource in the future. Clients such as search engines should remove the resource from their indices.[40] Most use cases do not require clients and search engines to purge the resource, and a "404 Not Found" may be used instead.
+	 */
 	Gone(410, "The requested resource is no longer available on the server and no redirect address is available."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request did not specify the length of its content, which is required by the requested resource.
+	 */
 	Length_Required(411, "The server will not accept the request without a Content-Length field."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server does not meet one of the preconditions that the requester put on the request.
+	 */
 	Precondition_Failed(412, "The supplied precondition evaluated to false on the server."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request is larger than the server is willing or able to process. Previously called "Request Entity Too Large".
+	 */
 	Request_Entity_Too_Large(413, "The request was unsuccessful because the request entity was larger than the server would allow"),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The URI provided was too long for the server to process. Often the result of too much data being encoded as a query-string of a GET request, in which case it should be converted to a POST request. Called "Request-URI Too Long" previously.
+	 */
 	Requested_URI_Too_Long(414, "The request was unsuccessful because the requested URI is longer than the server is willing to process (that's what she said)."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request entity has a media type which the server or resource does not support. For example, the client uploads an image as image/svg+xml, but the server requires that images use a different format.
+	 */
 	Unsupported_Media_Type(415, "The request was unsuccessful because the request was for an unsupported format."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The client has asked for a portion of the file (byte serving), but the server cannot supply that portion. For example, if the client asked for a part of the file that lies beyond the end of the file. Called "Requested Range Not Satisfiable" previously.
+	 */
 	Request_Range_Not_Satisfiable(416, "The range of the resource does not overlap with the values specified in the requests Range header field and not alternative If-Range field was supplied."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server cannot meet the requirements of the Expect request-header field.
+	 */
 	Expectation_Failed(417, "The expectation supplied in the Expectation header field could not be met by the server."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * This code was defined in 1998 as one of the traditional IETF April Fools' jokes, in RFC 2324, Hyper Text Coffee Pot Control Protocol, and is not expected to be implemented by actual HTTP servers. The RFC specifies this code should be returned by teapots requested to brew coffee. This HTTP status is used as an Easter egg in some websites, including Google.com.
+	 */
 	Im_A_Teapot(418, "I'm a teapot (IETF April Fools' jokes)"),
 	/**
-	 * Spring Framework
+	 * <b>Client Error</b> <br>
+	 * (Spring Framework) A deprecated response used by the Spring Framework when a method has failed.
 	 */
 	Method_Failure(420, "A deprecated response when a method has failed."),
 	/**
-	 * version 1 of the Twitter Search and Trends API
+	 * <b>Client Error</b> <br>
+	 * (Twitter) Returned by version 1 of the Twitter Search and Trends API when the client is being rate limited; versions 1.1 and later use the 429 Too Many Requests response code instead.
 	 */
 	Enhance_Your_Calm(420, "The client is being rate limited"),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request was directed at a server that is not able to produce a response (for example because a connection reuse).
+	 */
 	Misdirected_Request(421, "The request was directed at a server that is not able to produce a response (for example because a connection reuse)"),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request was well-formed but was unable to be followed due to semantic errors.
+	 */
 	Unprocessed_Entity(422, "The request was well-formed but was unable to be followed due to semantic errors."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The resource that is being accessed is locked.
+	 */
 	Locked(423, "The resource that is being accessed is locked."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The request failed due to failure of a previous request (e.g., a PROPPATCH).
+	 */
 	Failed_Dependency(424, "The request failed due to failure of a previous request."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The client should switch to a different protocol such as TLS/1.0, given in the Upgrade header field.
+	 */
 	Upgrade_Required(426, "The client should switch to a different protocol , given in the Upgrade header field."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The origin server requires the request to be conditional. Intended to prevent "the 'lost update' problem, where a client GETs a resource's state, modifies it, and PUTs it back to the server, when meanwhile a third party has modified the state on the server, leading to a conflict."
+	 */
 	Precondition_Required(428, "The origin server requires the request to be conditional. Intended to prevent the lost update problem."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The user has sent too many requests in a given amount of time. Intended for use with rate-limiting schemes.
+	 */
 	Too_Many_Requests(429, "The user has sent too many requests in a given amount of time. Intended for use with rate-limiting schemes."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server is unwilling to process the request because either an individual header field, or all the header fields collectively, are too large.
+	 */
 	Request_Header_Fields_Too_Large(431, "The server is unwilling to process the request because either an individual header field, or all the header fields, are too large."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The client's session has expired and must log in again.
+	 */
 	Login_Time_Out(440, "The client's session has expired."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * (nginx) Used to indicate that the server has returned no information to the client and closed the connection.
+	 */
+	No_Response(444, "returned no information to the client and closed the connection."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * The server cannot honour the request because the user has not provided the required information.
+	 */
 	Retry_With(449, "The server cannot honour the request because the user has not provided the required information."),
 	/**
-	 * Microsoft
+	 * <b>Client Error</b> <br>
+	 * (Microsoft) The Microsoft extension code indicated when Windows Parental Controls are turned on and are blocking access to the given webpage.
 	 */
 	Blocked_by_Windows_Parental_Controls(450, "when Windows Parental Controls are turned on and are blocking access to the given webpage."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * A server operator has received a legal demand to deny access to a resource or to a set of resources that includes the requested resource.[54] The code 451 was chosen as a reference to the novel Fahrenheit 451.
+	 */
 	Unavailable_For_Legal_Reasons(451, "A server operator has received a legal demand to deny access to a resource or to a set of resources that includes the requested resource."),
 	/**
-	 * Exchange ActiveSync
+	 * <b>Client Error</b> <br>
+	 * (Exchange ActiveSync) Used when either a more efficient server is available or the server cannot access the users' mailbox.[79] The client is expected to re-run the HTTP AutoDiscover operation to find a more appropriate server.
 	 */
 	Redirect(451, "Used when either a more efficient server is available or the server cannot access the users' mailbox."),
 	/**
-	 * (Esri) ArcGIS Server
+	 * <b>Client Error</b> <br>
+	 * (nginx) An expansion of the 400 Bad Request response code, used when the client has provided an invalid client certificate.
+	 */
+	SSL_Certificate_Error(495, "The client has provided an invalid client certificate."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * (nginx) An expansion of the 400 Bad Request response code, used when a client certificate is required but not provided.
+	 */
+	SSL_Certificate_Required(496, "A client certificate is required but not provided."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * (nginx) An expansion of the 400 Bad Request response code, used when the client has made a HTTP request to a port listening for HTTPS requests.
+	 */
+	HTTP_Request_Sent_to_HTTPS_Port(497, "client has made a HTTP request to a port listening for HTTPS requests."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * (ArcGIS Server) indicates an expired or otherwise invalid token.
 	 */
 	Invalid_Token(498, "indicates an expired or otherwise invalid token."),
 	/**
-	 * (Esri) ArcGIS Server
+	 * <b>Client Error</b> <br>
+	 * (ArcGIS Server) indicates that a token is required but was not submitted.
 	 */
 	Token_Required(499, "indicates that a token is required but was not submitted."),
+	/**
+	 * <b>Client Error</b> <br>
+	 * (nginx) Used when the client has closed the request before the server could send a response.
+	 */
+	Client_Closed_Request(499, "The client has closed the request before the server could send a response."),
 	
 	// --------------------------------------------------------------------
 	
+	/**
+	 * <b>Server Error</b> <br>
+	 * A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.
+	 */
 	Internal_Server_Error(500, "The request was unsuccessful because the server encountered an unexpected error."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server either does not recognize the request method, or it lacks the ability to fulfill the request. Usually this implies future availability (e.g., a new feature of a web-service API).
+	 */
 	Not_Implemented(501, "The server does not support the request."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server was acting as a gateway or proxy and received an invalid response from the upstream server.
+	 */
 	Bad_Gateway(502, "The server, whilst acting as a proxy, received an invalid response from the server that was fulfilling the request."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server is currently unavailable (because it is overloaded or down for maintenance). Generally, this is a temporary state.
+	 */
 	Service_Unavailable(503, "The request was unsuccessful as the server is either down or slash reedited."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server was acting as a gateway or proxy and did not receive a timely response from the upstream server.
+	 */
 	Gateway_Timeout(504, "The server, whilst acting as a proxy, did not receive a response from the upstream server in an acceptable time."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server does not support the HTTP protocol version used in the request.
+	 */
 	Http_Version_Not_Supported(505, "The server does not supported the HTTP protocol version specified in the request"),
-	
+	/**
+	 * <b>Server Error</b> <br>
+	 * Transparent content negotiation for the request results in a circular reference.
+	 */
 	Variant_Also_Negotiates(506, "Transparent content negotiation for the request results in a circular reference."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server is unable to store the representation needed to complete the request.
+	 */
 	Insufficient_Storage(507, "The server is unable to store the representation needed to complete the request."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The server detected an infinite loop while processing the request.
+	 */
 	Loop_Detected(508, "The server detected an infinite loop while processing the request."),
 	/**
-	 * shared hosting providers
+	 * <b>Server Error</b> <br>
+	 * The server has exceeded the bandwidth specified by the server administrator; this is often used by shared hosting providers to limit the bandwidth of customers.
 	 */
 	Bandwidth_Limit_Exceeded(509, "The server has exceeded the bandwidth specified by the server administrator."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * Further extensions to the request are required for the server to fulfill it.
+	 */
 	Not_Extended(510, "Further extensions to the request are required for the server to fulfill it."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * The client needs to authenticate to gain network access. Intended for use by intercepting proxies used to control access to the network (e.g., "captive portals" used to require agreement to Terms of Service before granting full Internet access via a Wi-Fi hotspot).
+	 */
 	Network_Authentication_Required(511, "The client needs to authenticate to gain network access."),
 	/**
-	 * the Pantheon web platform
+	 * <b>Server Error</b> <br>
+	 * (The Pantheon web platform) Used to indicate a site that has been frozen due to inactivity.
 	 */
 	Site_is_Frozen(530, "indicate a site that has been frozen due to inactivity."),
 	/**
-	 * some HTTP proxies
+	 * <b>Server Error</b> <br>
+	 * Used by some HTTP proxies to signal a network read timeout behind the proxy to a client in front of the proxy.
 	 */
 	Network_read_timeout_error(598, "to signal a network read timeout behind the proxy to a client in front of the proxy."),
-	
+	/**
+	 * <b>Server Error</b> <br>
+	 * Used to indicate when the connection to the network times out.
+	 */
 	Network_connect_timeout_error(599, "indicate when the connection to the network times out."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) The 520 error is used as a "catch-all response for when the origin server returns something unexpected", listing connection resets, large headers, and empty or invalid responses as common triggers.
+	 */
+	Unknown_Error(520, "server returns something unexpected."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) The origin server has refused the connection from Cloudflare.
+	 */
+	Web_Server_is_Down(521, "refused the connection."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Cloudflare could not negotiate a TCP handshake with the origin server.
+	 */
+	Connection_Timed_Out(522, "cannot negotiate a TCP handshake with the origin server."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Cloudflare could not reach the origin server; for example, if the DNS records for the origin server are incorrect.
+	 */
+	Origin_is_Unreachable(523, "cannot reach the origin server"),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Cloudflare was able to complete a TCP connection to the origin server, but did not receive a timely HTTP response.
+	 */
+	A_Timeout_Occurred(524, "did not receive a timely HTTP response."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Cloudflare could not negotiate a SSL/TLS handshake with the origin server.
+	 */
+	SSL_Handshake_Failed(525, "cannot negotiate a SSL/TLS handshake with the origin server."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Cloudflare could not validate the SSL/TLS certificate that the origin server presented.
+	 */
+	Invalid_SSL_Certificate(526, "cannot validate the SSL/TLS certificate that the origin server presented."),
+	/**
+	 * <b>Server Error</b> <br>
+	 * (Cloudflare) Error 527 indicates that the requests timeout or failed after the WAN connection has been established.
+	 */
+	Railgun_Error(527, "The requests timeout or failed after the WAN connection has been established."),
+	
 	
 	// --------------------------------------------------------------------
 	
+	/**
+	 * unknown code or not import yet.
+	 */
 	Unknown(999, "Unknown or unsupported HTTP status code");
 	
 	private final int code;
